4cd48cb1d962f45a3ddaff7be7e88e8086f60cf0
Git <elftocoo@gmail.com>
2019-01-20 17:54:52 +0900

    rebaseのとこでミスったので、修正した。

--- what_is_git.rb
+++ what_is_git.rb
@@ -1,9 +1,10 @@
 Class WhatIsGit
+  def about
+    puts 'https://ja.wikipedia.org/wiki/Git'
+  end
+
   def show
     puts 'Git is easy if you understand the basis.'
   end
 
-  def help
-    # コードは未定
-  end
 end
